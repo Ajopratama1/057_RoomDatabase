7b7f4a14244a300fd650d434dd2ab4c0de146a66
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-27 12:55:42 +0700

    Buat data class Siswa untuk tabel Room Database

--- app/src/main/java/com/example/a057_roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/a057_roomdatabase/room/Siswa.kt
@@ -3,6 +3,10 @@ package com.example.a057_roomdatabase.room
 import androidx.room.PrimaryKey
 
 data class Siswa(
-
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
 
 )
