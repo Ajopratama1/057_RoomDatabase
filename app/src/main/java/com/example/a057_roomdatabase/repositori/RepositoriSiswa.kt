736ab559e54554092aeeda6c984b0b2492f011e7
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-27 14:52:32 +0700

    Ubah nama interface dan class repositori
    Mengubah nama `RoomRepositoryInterface` menjadi `RepositoriSiswa` dan `OfflineRepository` menjadi `OfflineRepositori` untuk konsistensi penamaan.

--- app/src/main/java/com/example/a057_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a057_roomdatabase/repositori/RepositoriSiswa.kt
@@ -4,14 +4,14 @@ import com.example.a057_roomdatabase.room.Siswa
 import com.example.a057_roomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
-interface RoomRepositoryInterface {
+interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 }
 
-class OfflineRepository(
+class OfflineRepositori(
     private val siswaDao: SiswaDao
-) : RoomRepositoryInterface {
+) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
