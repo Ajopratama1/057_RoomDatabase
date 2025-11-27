92cc987d5398994a018a9261ceade1dd7e75e273
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-27 14:42:03 +0700

    memperbaiki error

--- app/src/main/java/com/example/a057_roomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/a057_roomdatabase/view/route/DestinasiHome.kt
@@ -2,7 +2,8 @@ package com.example.a057_roomdatabase.view.route
 
 import com.example.a057_roomdatabase.R
 
-object DatabaseSiswa : DestinasiNavigasi {
-    override val route ="home"
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
     override val titleRes = R.string.app_name
 }
