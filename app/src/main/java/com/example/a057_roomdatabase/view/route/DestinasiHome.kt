e1ce632a83941d39bf2c87054dabf29c88701c71
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-27 13:31:07 +0700

    Buat objek DatabaseSiswa sebagai rute Home (halaman utama) navigasi

---
+++ app/src/main/java/com/example/a057_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a057_roomdatabase.view.route
+
+import com.example.a057_roomdatabase.R
+
+object DatabaseSiswa : DestinasiNavigasi {
+    override val route ="home"
+    override val titleRes = R.string.app_name
+}
